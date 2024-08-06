e612712769877eb0d4dd6e86f0f2127b8dbd6f01
vincent gannier <vincent.gannier@gmail.com>
2024-08-06 16:33:39 +0200

    new

--- db/migrate/20240806133502_create_articles.rb
+++ db/migrate/20240806133502_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[7.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :content.text
+      t.text :content
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  article = Article.new(
+    title: Faker::Company.name,
+    content: Faker::Quote.yoda
+  )
+  article.save!
+end
